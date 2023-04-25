f24a7689075434c6f80ce9700b216e6ae3ea6a7a
shadyjal <131809092+shadyjal@users.noreply.github.com>
2023-04-25 14:31:42 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
-PRESIGNED_URL=""             # replace with presigned url from email
+PRESIGNED_URL="https://dobf1k6cxlizq.cloudfront.net/*?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9kb2JmMWs2Y3hsaXpxLmNsb3VkZnJvbnQubmV0LyoiLCJDb25kaXRpb24iOnsiRGF0ZUxlc3NUaGFuIjp7IkFXUzpFcG9jaFRpbWUiOjE2ODI1MjYyODN9fX1dfQ__&Signature=rO0N2AuEL~~oFZQIzZIq6-r5BliLvUXDNSiX3ND6myZtegP2t4wNfOBeO1cFH8qx4PsU4ie13EBZNOGZBE7iHqx747pP6U3D8ojbGe38W4QeX9BDsSbgYJBw-IrP~2cG8pw6QDnn2TPVdKlUsQGio8IbhOvAwhvQOlMKvXQKnkZlUTRI~MVhAG5n6Ra7LV9cyRXABnsg6y95rSTJQYkdEUwoj2AAyhmuQs~1F5GNRkS4QsolbiUXVnsjd76~LezpEgCPzVHafAH2s71zlYTmtzlP8hXPiukSbEFLH54uU~rahlfW2zCB5jPB1vaCrTxIXNxkV-ZtGd~ddkwaTS5zxQ__&Key-Pair-Id=K231VYXPC1TA1R"             # replace with presigned url from email
 MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
 TARGET_FOLDER=""             # where all files should end up
 
